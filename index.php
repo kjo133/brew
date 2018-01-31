93d41b9d4bc1c741d24ec0df0672b43206246225
Kevin Olson <kolson@si6942.si.net>
2018-01-31 14:52:27 -0500

    First Brew :

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sue's Bruise</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+
+
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow|Maven+Pro" rel="stylesheet">
+    <!-- <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.css"> -->
+   <!--  <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet"> -->
+</head>
+
+<body>
+   
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+           
+            <li><a href="#">Sue's Bruise</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#about">About</a></li>
+            <li><a href="#ourStuff">News</a></li>
+            <li><a href="#schedule">Menu</a></li>
+            <!-- <li><a target="_blank" href="https://www.instagram.com/thesprintingturtles/"><i class="fa fa-lg fa-instagram"></i></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/thesprintingturtles" ><i class="fa fa-lg fa-facebook"></i></a></li> -->
+            
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+     <div class="hero">
+        
+         <!-- <h1 >Sue's Bruise</h1> -->
+         <div id="heroLogo">
+         <h1>Sue's Bruise</h1>
+        </div> 
+       
+     </div>
+     <br />
+     <!-- about -->
+     <div class="container">
+         <div id="about">
+             <h1>Our Story</h1>
+             <hr>
+             <p>
+                 Sue's Bruise is a brewery originally started in an apartment in Oakton, Virginia.  Sue, the head brewmaster soon set up shop in a slightly larger location.  Today Sue's Bruise is a team of two currently brewing in Vienna, Virginia.  The operation is small with us only able to brew a gallon per session but we have goals of expanding to a full size facility in the future.
+             </p>
+             <br />
+             <h1>Hours and Schedule</h1>
+             <hr>
+             <p>Being a small operation we are currently only offering tours on Saturdays from 12pm-1pm.
+                <br/>
+                Our normal hours where you can come and buy and drink beer are as followed
+                <ul>
+                    <li>Monday-Thursday 6:00pm - 8:00pm</li>
+                    <li>Friday-Saturday 6:00pm - 10:00pm</li>
+                    <li>Sunday - 4:00pm - 6:00pm</li>
+                </ul>
+                Please check our events for any additional hours.
+              </p>
+         </div>
+    </div>
+    <!-- end about -->
+    <!-- team -->
+    <div class="team">
+        <div class="container">
+            <h1>Team</h1>
+            <hr>
+            <div class="col-md-6 teammember">
+                <img src="images/susie.jpg">
+                <h2>Susie Hoopes</h2>
+                <p>Head Brewmaster</p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-md-6 teammember">
+                <a href="http://www.kevinjolson.com" target="_blank"><img src="images/kevin.jpg"></a>
+                <h2>Kevin Olson</h2>
+                <p>Assistant Brewer</p>
+            </div><!-- /.col-lg-4 -->
+        </div>
+    </div>
+
+</body>
+</html>
